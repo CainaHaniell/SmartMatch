6071c8629b4dcc57ad25cfa3cfefa7cced88eadf
Cainã Haniel <cainaoriginal96@gmail.com>
2025-06-21 19:44:07 -0300

    Reforma

--- navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".hamburguer");
-const menuLinks = document.querySelector(".menu-links");
-
-hamburger.addEventListener("click", () => {
-  menuLinks.classList.toggle("active");
-});
